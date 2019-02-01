0e73d76d3713ebd01f5d3805c93217a155ca7431
杨帆 <yangfanvw@qq.com>
2019-02-01 19:23:20 +0800

    修改Unwind Segue的使用

--- Unwind Segue使用/Unwind Segue使用/ViewController.m
+++ Unwind Segue使用/Unwind Segue使用/ViewController.m
@@ -6,10 +6,11 @@
 //  Copyright © 2018年 student. All rights reserved.
 
 
+//具体传值步骤
 //1. SB中完成两个界面的正向跳转
 //2. 在获取值的界面写一个类似如右的方法。  -(IBAction)getValue:(UIStoryboardSegue *)sender
 //3. 在该方法中进行判断和获取下个界面传过来的值
-//4. SB中选中下个界面的Exit，并于2中的方法连线
+//4. SB中选中下个界面的Exit，并和2中的方法连线
 //5. 连线以后左侧会多一个Unwind Segue，选中它设置identifier
 
 #import "ViewController.h"
@@ -40,8 +41,10 @@
     
     if ([sender.identifier isEqualToString:@"unwindAB"]) {
         
+        //注意这里是sourceViewController 不是destinationViewController
         SecondViewController *svc = sender.sourceViewController;
         
+        //获取值
         self.strValue.text = svc.returnData;
 
     }
